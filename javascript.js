65f732d7444d2622519147fff70563a808109f85
debo <ogunweoluwadebo@gmail.com>
2022-07-06 15:22:01 +0100

    update done to sign-up page

--- javascript.js
+++ javascript.js
@@ -20,13 +20,30 @@ const signup = ()=>{
         userContact:[],
         userNote:[],
     }
-    detai.push(informat);
-    localStorage.member = JSON.stringify(detai);
-    console.log(detai);
-    firstname.value = "";
-    lastname.value = "";
-    address.value = "";
-    email.value = "";
-    number.value = "";
-    location.assign("signin.html");
+    if(firstname.value == "" ||  lastname.value == "" || address.value == ""  || email.value == "" || password.value == "" || number.value == "" || gd2 == ""){
+        message1.innerHTML = "Enter the value of the firstname"
+        message1.style.color = "red";
+        message2.innerHTML = "Enter the value of the Lastname"
+        message2.style.color = "red";
+        message3.innerHTML = "Enter the value of the address"
+        message3.style.color = "red";
+        message4.innerHTML = "Enter the value of the email"
+        message4.style.color = "red";
+        message5.innerHTML = "Enter the value of the password"
+        message5.style.color = "red";
+        message6.innerHTML = "Enter the value of the phoneNumber"
+        message6.style.color = "red";
+        message7.innerHTML = "Choose your gender"
+        message7.style.color = "red";
+    }else{
+        detai.push(informat);
+        localStorage.member = JSON.stringify(detai);
+        console.log(detai);
+        firstname.value = "";
+        lastname.value = "";
+        address.value = "";
+        email.value = "";
+        number.value = "";
+        location.assign("signin.html");
+    }
 }
